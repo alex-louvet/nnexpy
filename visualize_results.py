31cb301cf843f0cb3d68f9bfbaf5112067b3573e
Alexandre LOUVET <alexandre.louvet8@gmail.com>
2020-03-19 13:42:25 +0900

    integrate betti numbers in testing

--- visualize_results.py
+++ visualize_results.py
@@ -6,11 +6,12 @@ from sklearn.utils import shuffle
 import pandas as pd
 from os import walk, path
 import matplotlib.pyplot as plt
+import gudhi
 
 # change path to visualize different models
-mypath = "./models/H(1,0,1)/"
+mypath = "./models/D(0,2,0)/"
 
-with open('./models/data_descriptor.pkl', 'rb') as input:
+with open(mypath + 'data_descriptor.pkl', 'rb') as input:
     centerList = pickle.load(input)
     radiusList = pickle.load(input)
     bounds = pickle.load(input)
@@ -20,6 +21,7 @@ dataDescriptor = DataDescriptor(nHoles=len(centerList), centerList=centerList,
                                 radiusList=radiusList, random=randomSeed, bounds=bounds)
 
 instance = dataDescriptor.generateData(classNumber=2, pointsNumber=50000)
+print(instance.bettiNumbers())
 instance.plot()
 instance_test = dataDescriptor.generateData(classNumber=2, pointsNumber=50000)
 test = dataDescriptor.generateTestData(pointsNumber=50000)
@@ -31,19 +33,23 @@ model2 = keras.models.load_model(mypath + '2layers.h5')
 model4 = keras.models.load_model(mypath + '4layers.h5')
 model8 = keras.models.load_model(mypath + '8layers.h5')
 
-predictedTest = test.predict(model1, verbose=0)
+predictedTest = instance_test.predict(model1, verbose=0)
+print(predictedTest.bettiNumbers(nPoints=10000))
 predictedTest.plot()
 predictedAccuracy = instance_test.predict_and_evaluate(model1, verbose=0)
 print(predictedAccuracy)
 predictedTest = test.predict(model2, verbose=0)
+print(predictedTest.bettiNumbers(nPoints=10000))
 predictedTest.plot()
 predictedAccuracy = instance_test.predict_and_evaluate(model2, verbose=0)
 print(predictedAccuracy)
 predictedTest = test.predict(model4, verbose=0)
+print(predictedTest.bettiNumbers(nPoints=10000))
 predictedTest.plot()
 predictedAccuracy = instance_test.predict_and_evaluate(model4, verbose=0)
 print(predictedAccuracy)
 predictedTest = test.predict(model8, verbose=0)
+print(predictedTest.bettiNumbers(nPoints=10000))
 predictedTest.plot()
 predictedAccuracy = instance_test.predict_and_evaluate(model8, verbose=0)
 print(predictedAccuracy)
