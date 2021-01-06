1a89740dd226d2fb08dd28dd9be532b2152659e1
Alexandre Louvet <alexandre.louvet8@gmail.com>
2021-01-06 14:46:31 +0900

    remove test nnexpy from examples

--- examples/nnexpy/__init__.py
+++
@@ -1,4 +0,0 @@
-from .homology_data_generation import DataDescriptor, DataInstance, DataPoint, Bounds
-from .network_generator import NetworkGenerator
-from .random_networks import RandomNetwork
-from .trajectories import KnotDescriptor
